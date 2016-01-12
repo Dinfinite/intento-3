003eab9d4d86207d92ed1ba2a51468f9582a6fb4
Diego Perez <superdiegoshowdown@gmail.com>
2016-01-12 12:35:32 -0600

    hola

--- tarea 1/DetailViewController.swift
+++ tarea 1/DetailViewController.swift
@@ -43,3 +43,25 @@ class DetailViewController: UIViewController {
 
 }
 
+var x = 0...100
+
+for r in x {
+    
+    if  r % 5 == 0 {
+        print ("\(r)  Bingo!!!")
+    }
+    else if r >= 30 && r <= 40 {
+        print ("\(r)  VivaSwift!!!")
+    }
+        
+    else if r % 2 == 0 {
+        print ("\(r)  par!!!")
+    }
+    else if r % 2 != 0 {
+        print ("\(r)  impar!!!")
+        
+        
+        
+    }
+}
+
---
+++ tarea1_1.swift
@@ -0,0 +1,31 @@
+//
+//  tarea1_1.swift
+//  tarea 1
+//
+//  Created by Diego Pérez on 1/11/16.
+//  Copyright © 2016 Diego Pérez. All rights reserved.
+//
+
+import Foundation
+
+    var x = 0...100
+
+    for r in x {
+    
+    if  r % 5 == 0 {
+        print ("\(r)  Bingo!!!")
+    }
+    else if r >= 30 && r <= 40 {
+        print ("\(r)  VivaSwift!!!")
+    }
+        
+    else if r % 2 == 0 {
+        print ("\(r)  par!!!")
+    }
+    else if r % 2 != 0 {
+        print ("\(r)  impar!!!")
+        
+        
+        
+    }
+}
